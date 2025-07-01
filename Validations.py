f57afb75613827ba6075b5782073c375e2fa2080
Justin Lipner <jul155@pitt.edu>
2025-07-01 12:29:29 -0400

    Small changes.

--- Validations.py
+++ Validations.py
@@ -8,13 +8,14 @@ def Create4NodeSystem():
   circ = Circuit("4nodebus")
   print(circ.name)
 
-  circ.add_bus("bus1", 1)
-  circ.add_bus("bus2", 1)
-  circ.add_bus("bus3", 1)
-  circ.add_bus("bus4", 1)
+  circ.add_bus("bus1", 12.47)
+  circ.add_bus("bus2", 12.47)
+  circ.add_bus("bus3", 12.47)
 
-  phase_conductor = circ.add_conductor("1/0_ACSR", 0.398, 0.00446, 1.12, 230)
-  print(circ.conductors["1/0_ACSR"].GMR)
-  neutral_conductor = circ.add_conductor("1/0_ACSR_neutral", 0.398, 0.00446, 1.12, 230)
-  #geometry1 = circ.add_geometry("Geometry 1", [0+j*29, 7+j*29, 2.5+j*29, 4+j*25], 3, phase_conductor, neutral_conductor)
-  #line1 = circ.add_dline_from_geometry("OH1", "bus1", "bus2", geometry1, 1.893939)
+  circ.add_conductor("1/0_ACSR", 0.398, 0.00446, 1.12, 230)
+  circ.add_conductor("1/0_ACSR_neutral", 0.398, 0.00446, 1.12, 230)
+  circ.add_geometry("Geometry 1", [0+j*29, 7+j*29, 2.5+j*29, 4+j*25], 3, circ.conductors["1/0_ACSR"], circ.conductors["1/0_ACSR_neutral"])
+  line1 = circ.add_dline_from_geometry("line1", "bus1", "bus2", "Geometry 1", 0.3787879)
+  line2 = circ.add_dline_from_geometry("line2", "bus2", "bus3", "Geometry 1", 0.4734848)
+  
+  circ.do_fbsweep()
