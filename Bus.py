74d970203e70521e48dafdf281482e9ed98999f6
Justin Lipner <jul155@pitt.edu>
2025-07-01 12:25:35 -0400

    Refactored bus class to use three phase quantities.

--- Bus.py
+++ Bus.py
@@ -9,72 +9,33 @@ Date: 2025-01-23
 
 class Bus:
     """
-    Bus class to implement bus functionality
+    Bus class supporting unbalanced, multiphase operation.
     """
-    def __init__(self, name: str, base_kv: float, index: int):
+    def __init__(self, name: str, base_kv: float, index: int, phases=None):
         """
-        Constructor for bus class
-        :param name: Name of bus
-        :param base_kv: Base voltage of bus
-        :param index: Index of bus
+        :param name:   Name of bus
+        :param base_kv: Base voltage of bus (line-to-neutral) in kV
+        :param index:  Index of bus
+        :param phases: Iterable of phase labels, e.g. ['A','B','C']
         """
         self.name = name
-        self.base_kv = base_kv*1e3
+        self.base_kv = base_kv * 1e3  # convert to V
         self.index = index
-        self.Vpu = 1 # per unit voltage
-        self.V = base_kv # actual voltage.
-        self.angle = 0.0
-        self.real_power = 0.0
-        self.reactive_power = 0.0
-        self.type = "PQ" # bus type
-    
 
-    def set_bus_v(self, v: float):
-        """
-        Set function for Vpu
-        :param v:
-        :return:
-        """
-        self.Vpu = v
-        self.V = self.base_kv*v
-    
+        # default to three-phase
+        self.phases = ['A','B','C'] if phases is None else phases
 
-    def set_angle(self, a: float):
-        """
-        Set function for angle
-        :param a:
-        :return:
-        """
-        self.angle = a
-
-
-    def set_type(self, t: str):
-        """
-        Set function for type
-        :param t:
-        :return:
-        """
-        self.type = t
-    
-
-    def set_power(self, real: float, reactive: float):
-        """
-        Set function for power
-        :param real:
-        :param reactive:
-        :return:
-        """
-        self.real_power += real
-        self.reactive_power += reactive
+        # initialize per-phase quantities
+        self.Vpu   = {phase: 1.0     for phase in self.phases}
+        self.V     = {phase: self.base_kv for phase in self.phases}
+        self.angle = {phase: 0.0     for phase in self.phases}
+        self.P     = {phase: 0.0     for phase in self.phases}  # real power injection (W)
+        self.Q     = {phase: 0.0     for phase in self.phases}  # reactive power injection (var)
+        self.type  = "PQ"  # you might also want per-phase types
 
 
 # validation tests
 if __name__ == '__main__':
     from Bus import Bus
     bus1 = Bus("Bus 1", 20, 1)
-    bus2 = Bus("Bus 2", 230, 2)
-    print(bus1.name, bus1.base_kv, bus1.index)
-    print(bus2.name, bus2.base_kv, bus2.index)
-
-    bus1.set_bus_v(500)
-    print(bus1.base_kv)
+    print(bus1.name, bus1.base_kv, bus1.index, bus1.V)
