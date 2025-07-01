38d40ab7ff05323cedebd2c86cbe56ed88310270
Justin Lipner <jul155@pitt.edu>
2025-07-01 12:29:17 -0400

    Began working on LIT solver. WIP.

--- Solution.py
+++ Solution.py
@@ -1,9 +1,33 @@
 from Circuit import Circuit
 import numpy as np
-import pandas as pd
 
-class Solution:
+class LIT:
 
   def __init__(self, circ: Circuit):
     self.circ = circ
+    self.numbuses = len(self.circ.buses)
+    self.a = {}
+    self.b = {}
+    self.c = {}
+    self.d = {}
+    self.Iload = {}
+
+  
+  def setup(self):
+    for line in self.circ.distribution_lines.values():
+      self.a.update({line.name: np.identity(3) + 0.5*np.matmul(line.Zabc, line.Yabc)})
+      self.b.update({line.name: line.Zabc})
+      self.c.update({line.name: line.Yabc + 0.25*np.matmul(line.Yabc, np.matmul(line.Zabc, line.Yabc))})
+      self.d.update({line.name: np.identity(3) + 0.5*np.matmul(line.Zabc, line.Yabc)})
     
+    
+  def lit(self):
+    self.setup()
+
+
+  def forward_sweep(self):
+    pass
+
+
+  def backward_sweep(self):
+    pass
