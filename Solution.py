3c369ee90aa7a86d0a1f7c8cee6c5330a8a26b12
Justin Lipner <jul155@pitt.edu>
2025-07-14 16:00:34 -0400

    Forward/backward sweep has been implemented, but results don't match OpenDSS. WIP.

--- Solution.py
+++ Solution.py
@@ -1,5 +1,5 @@
 from Circuit import Circuit
-from math import sqrt, sin, cos
+from math import sqrt
 import numpy as np
 j = 1j
 
@@ -30,79 +30,48 @@ class LIT:
     for i in range(self.numbuses):
       self.Iabc.update({f"I{i+1}": np.array([0, 0, 0], dtype=complex)})
 
-    V0 = self.circ.buses[self.circ.slack_bus].base_kv/sqrt(3)
-    for i in range(self.numbuses):
-      self.VLGabc.update({f"V{i+1}": np.array([V0, V0*np.exp(j*-2*np.pi/3), V0*np.exp(j*2*np.pi/3)], dtype=complex)})
-    
-  
-  def lit1(self):
+    for bus in self.circ.buses.values():
+      V0 = bus.base_kv/sqrt(3)
+      self.VLGabc.update({f"V{bus.index}": np.array([V0, V0*np.exp(j*-2*np.pi/3), V0*np.exp(j*2*np.pi/3)], dtype=complex)})
+
+
+  def lit(self):
     self.setup()
-    iters = 4
+    iters = 200
     for i in range(iters):
-      self.forward_sweep_project1()
-      self.backward_sweep_project1()
+      self.forward_sweep_project()
+      self.backward_sweep_project()
     
     return self.VLGabc, self.Iabc
 
 
-  def lit2(self):
-    self.setup()
-    iters = 4
-    for i in range(iters):
-      self.forward_sweep_project2()
-      self.backward_sweep_project2()
-    
-    return self.VLGabc, self.Iabc
-  
-  
-  def forward_sweep_project1(self):
+  def forward_sweep_project(self):
     VLGabc2 = np.matmul(self.A["line1"], self.VLGabc["V1"]) - np.matmul(self.B["line1"], self.Iabc["I2"])
-    self.VLGabc["V2"] = VLGabc2
-
-
-  def forward_sweep_project2(self):
-    VLGabc2 = np.matmul(self.A["line1"], self.VLGabc["V1"]) - np.matmul(self.B["line1"], self.Iabc["I2"])
-    VLGabc3 = np.matmul(self.A["line2"], self.VLGabc["V2"]) - np.matmul(self.B["line2"], self.Iabc["I3"])
+    VLGabc3 = np.matmul(self.circ.transformers["T1"].At, self.VLGabc["V2"]) - np.matmul(self.circ.transformers["T1"].Bt, self.Iabc["I3"])
+    VLGabc4 = np.matmul(self.A["line2"], self.VLGabc["V3"]) - np.matmul(self.B["line2"], self.Iabc["I4"])
     self.VLGabc["V2"] = VLGabc2
     self.VLGabc["V3"] = VLGabc3
-
-
-  def backward_sweep_project1(self):
-    # bus 2 (PQ load on all 3 phases)
-    load2  = self.circ.loads["load1"]
-    S2 = load2.S
-    V2 = self.VLGabc["V2"]
-    Iload2 = np.conjugate(S2 / V2) # vector of 3 currents
-
-    # build I1 from line1 using I2
-    I1 = ( self.c["line1"] @ self.VLGabc["V2"]
-          + self.d["line1"] @ Iload2 ) + Iload2
-
-    self.Iabc["I1"] = I1 
-    self.Iabc["I2"] = Iload2
+    self.VLGabc["V4"] = VLGabc4
 
   
-  def backward_sweep_project2(self):
-    # bus 3 (constant‐Z, phase B only)
-    load3 = self.circ.loads["load2"]
-    Sb = load3.S[1]                                  
-    Vph = self.circ.buses[self.circ.slack_bus].base_kv/np.sqrt(3)
-    Zb = Vph**2 / Sb                             
-    I3 = np.array([0, self.VLGabc["V3"][1] / Zb, 0], dtype=complex)
+  def backward_sweep_project(self):
+    # calculating bus 4 load current
+    load1 = self.circ.loads["load1"]
+    S = load1.S                                  
+    VLGabc4 = self.VLGabc["V4"]                  
+    I4 = np.conjugate(S/VLGabc4)
+    self.Iabc["I4"] = I4
 
-    # bus 2 (PQ load on all 3 phases)
-    load2  = self.circ.loads["load1"]
-    S2 = load2.S
-    V2 = self.VLGabc["V2"]
-    Iload2 = np.conjugate(S2 / V2) # vector of 3 currents
-
-    # build I2 by adding line-shunt + load2 + I3 injection
-    I2 = ( self.c["line2"] @ self.VLGabc["V3"]
-          + self.d["line2"] @ I3
-          + Iload2 )
+    # calculating I3, current in line2
+    I3 = (self.c["line2"] @ self.VLGabc["V3"]
+          + self.d["line2"] @ I4)
+    self.Iabc["I3"] = I3
+    
+    # calculating I2, current into the primary side of the transformer
+    I2 = np.matmul(self.circ.transformers["T1"].dt, I3)
+    self.Iabc["I2"] = I2
 
     # build I1 from line1 using I2
-    I1 = ( self.c["line1"] @ self.VLGabc["V2"]
-          + self.d["line1"] @ I2 )
-
-    self.Iabc["I1"], self.Iabc["I2"], self.Iabc["I3"] = I1, I2, I3
+    I1 = (self.c["line1"] @ self.VLGabc["V2"]
+          + self.d["line1"] @ I2)
+    self.Iabc["I1"] = I1
