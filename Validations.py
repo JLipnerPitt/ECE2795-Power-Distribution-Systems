8fc4b820f35ef3a104f47be59a5b7b7a69528e03
Justin Lipner <jul155@pitt.edu>
2025-07-17 12:22:49 -0400

    Corrected input data for Project4.

--- Validations.py
+++ Validations.py
@@ -1,7 +1,4 @@
-import numpy as np
-
 from Circuit import Circuit
-from Settings import settings
 j = 1j
 
 def CreateProject4():
@@ -16,11 +13,13 @@ def CreateProject4():
 
   circ.add_conductor("336400_26/7_ACSR", 0.721, 0.0244, 0.306, 230)
   circ.add_conductor("4/0_6/1_ACSR", 0.563, 0.00814, 0.592, 230)
-  circ.add_geometry("Geometry 1", [0+j*29, 2.5+j*29, 7+j*29, 4+j*25], circ.conductors["336400_26/7_ACSR"], circ.conductors["4/0_6/1_ACSR"], [0, 1, 0])
-  circ.add_dline_from_geometry("line1", "bus1", "bus2", "Geometry 1", 0.3787879)
-  circ.add_dline_from_geometry("line2", "bus3", "bus4", "Geometry 1", 0.4734848)
-  circ.add_transformer("T1", "bus2", "bus3", 7.20, 2.40, [6000, 6000, 6000], [0.01, 0.01, 0.01], [0.06, 0.06, 0.06])
-  circ.add_load("load1", "bus4", [1800, 1800, 1800], [0.9, 0.9, 0.9])
+  circ.add_geometry("Geometry 1", [0+j*29, 2.5+j*29, 7+j*29, 4+j*25], circ.conductors["336400_26/7_ACSR"], circ.conductors["4/0_6/1_ACSR"], [1, 1, 1])
+  circ.add_dline_from_geometry("line1", "bus1", "bus2", "Geometry 1", 0.3787879, True)
+  circ.add_dline_from_geometry("line2", "bus3", "bus4", "Geometry 1", 0.4734848, True)
+  circ.add_transformer("T1", "bus2", "bus3", 7.20, 4.16, [6000, 6000, 6000], [0.01, 0.01, 0.01], [0.06, 0.06, 0.06])
+  circ.add_load("load1", "bus4", [2000, 2000, 2000], [0.9, 0.9, 0.9])
+
+  #print(circ.distribution_lines["line1"].Zabc)
 
   circ.do_fbsweep()
   
