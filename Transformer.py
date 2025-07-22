36ef757b793f35da09d7b76bfead524ec915da02
Justin Lipner <jul155@pitt.edu>
2025-07-22 13:29:07 -0400

    Finished Transformer class.

--- Transformer.py
+++ Transformer.py
@@ -16,7 +16,7 @@ class Transformer:
     Transformer class to hold transformer information
     """
 
-    def __init__(self, name: str, bus1: str, bus2: str, VprimLN: float, VsecLN: float, power_rating: list[float],
+    def __init__(self, name: str, bus1: str, bus2: str, VprimLL: float, VsecLL: float, power_rating: list[float],
                  resistance_percent: list[float], reactance_percent: list[float]):
         """
         Constructor for Transformer objects
@@ -31,13 +31,13 @@ class Transformer:
         self.name = name
         self.bus1 = bus1
         self.bus2 = bus2
-        self.VprimLN = VprimLN*1e3
-        self.VsecLN = VsecLN*1e3
+        self.VprimLN = VprimLL*1e3/sqrt(3)
+        self.VsecLL = VsecLL*1e3
         self.power_rating = np.array(power_rating)*1e3
         self.resistance_percent = resistance_percent
         self.reactance_percent = reactance_percent
 
-        self.n = self.VprimLN/self.VsecLN
+        self.n = self.VprimLN/self.VsecLL
         self.Ztabc = self.calc_Zt()
         self.at = self.n*np.array([[1, -1, 0], [0, 1, -1], [-1, 0, 1]])
         self.bt = self.calc_bt()
@@ -49,7 +49,7 @@ class Transformer:
 
     def calc_Zt(self):
         #X = X*settings.powerbase/self.power_rating  # updating pu to system power base
-        Zbase = [self.VsecLN**2/self.power_rating[0], self.VsecLN**2/self.power_rating[1], self.VsecLN**2/self.power_rating[2]]
+        Zbase = [self.VsecLL**2/self.power_rating[0], self.VsecLL**2/self.power_rating[1], self.VsecLL**2/self.power_rating[2]]
         Zab = Zbase[0]*(self.resistance_percent[0] + j*self.reactance_percent[0])
         Zbc = Zbase[1]*(self.resistance_percent[1] + j*self.reactance_percent[1])
         Zca = Zbase[2]*(self.resistance_percent[2] + j*self.reactance_percent[2])
@@ -78,7 +78,7 @@ class Transformer:
 if __name__ == '__main__':
     from Transformer import Transformer
 
-    transformer1 = Transformer("T1", "bus2", "bus3", 7.2, 0.24, [100, 50, 50], [0.01, 0.015, 0.015], [0.04, 0.035, 0.035])
+    transformer1 = Transformer("T1", "bus2", "bus3", 12.47, 0.24, [100, 50, 50], [0.01, 0.015, 0.015], [0.04, 0.035, 0.035])
     print(f"nt = {transformer1.n}")
     print()
     print(f"Ztabc = {transformer1.Ztabc}")
