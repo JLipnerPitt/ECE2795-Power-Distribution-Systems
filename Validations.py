24a2d42abd94864e0b029f534d8b111a6aa4da5a
Justin Lipner <jul155@pitt.edu>
2025-07-23 16:53:12 -0400

    Added Example 8.3 from the book.

--- Validations.py
+++ Validations.py
@@ -36,6 +36,7 @@ def CreateProject4_Balanced_Loads():
   circ.do_fbsweep()
   print()
 
+
 def CreateProject4_Unbalanced_Loads():
   circ = Circuit("Project4_unbalanced")
   print(circ.name)
@@ -58,4 +59,57 @@ def CreateProject4_Unbalanced_Loads():
 
   circ.do_fbsweep()
   print()
+
+
+def CreateExample8_3():
+  from math import sqrt
+  circ = Circuit("Exmaple 8.3")
+  print(circ.name)
+
+  circ.add_bus("bus1", 12.47)
+  circ.add_bus("bus2", 240)
+  circ.add_transformer("T1", "bus1", "bus2", 12.47, 0.240, [100, 50, 50], [0.01, 0.015, 0.015], [0.04, 0.035, 0.035])
+  circ.add_load("load1", "bus2", [100, 50, 50], [0.9, 0.8, 0.8])
+
+  Iabc = {}
+  Voltages = {}
+  Di = np.array([[1, 0, -1], [-1, 1, 0], [0, -1, 1]])
+  Dv = np.array([[1, -1, 0], [0, 1, -1], [-1, 0, 1]])
+  W = (1/3)*np.array([[2, 1, 0], [0, 2, 1], [1, 0, 2]])
+
+  Iabc.update({"I12": np.array([0, 0, 0], dtype=complex)})
+  Voltages.update({"V1": (1e3/sqrt(3))*np.array([12.47, 12.47*np.exp(-j*2*np.pi/3), 12.47*np.exp(j*2*np.pi/3)], dtype=complex)})
+  Voltages.update({"V2": np.array([240, 240*np.exp(-j*2*np.pi/3), 240*np.exp(j*2*np.pi/3)], dtype=complex)})
+  print("VLLabc =", np.abs(Voltages["V2"]))
+
+  iters = 1
+  VLLabc2 = Voltages["V2"]
+
+  # calculating delta load currents
+  load1 = circ.loads["load1"]
+  S = load1.S
+  print("SDabc =", S)                                                 
+  IDabc = np.conjugate(S/Voltages["V2"])
+  print("IDabc =", np.abs(IDabc))
+
+  # calculating secondary line currents
+  Iabc = Di @ IDabc
+  print("Iabc =", np.abs(Iabc))
+
+  # calculating the equivalent secondary line to neutral voltages
+  VLNabc2 = W @ VLLabc2
+  print("VLNabc2 =", np.abs(VLNabc2))
+
+  VLNABC2 = circ.transformers["T1"].at @ VLNabc2 + circ.transformers["T1"].bt @ Iabc
+  print("VLNABC2 =", np.abs(VLNABC2))
+
+  VLLABC2 = Dv @ VLNABC2
+  print("VLLABC2 =", np.abs(VLLABC2))
+
+  # calculating IABC, current into the primary side of the transformer
+  IABC = np.matmul(circ.transformers["T1"].dt, Iabc)
+  print("IABC =", np.abs(IABC))
+  print()
+
+
   
